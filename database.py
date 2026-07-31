492996804ecde4551ff7eea6957da7c5e7b64f1a
yk7776606-cyber <yk7776606@gmail.com>
2026-07-31 15:45:19 +0530

    Switch to PostgreSQL

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
-import mysql.connector
+import psycopg2
+import os
 
 def get_connection():
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="library_management"
+    connection = psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT", "5432")
     )
     return connection
