312637fc892f70ba846053fc09b50ab82956707b
yk7776606-cyber <yk7776606@gmail.com>
2026-08-15 08:59:58 +0530

    Adjusted librarian seal position

--- app.py
+++ app.py
@@ -1031,7 +1031,7 @@ def download_report():
         seal_width = 110
         seal_height = 110
 
-        seal_x = (width - seal_width) / 2
+        seal_x = (width - seal_width) / 2 - 30
         seal_y = 75
 
         pdf.drawImage(
